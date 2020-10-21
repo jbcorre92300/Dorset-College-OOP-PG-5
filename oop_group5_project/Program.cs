e886d1cd52b7a8baa9d6b1e92ba15a3f2ed9a1fc
JB Corre <23182@student.dorset-college.ie>
2020-10-21 18:19:15 +0100

    Ajoutez des fichiers projet.

---
+++ oop_group5_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_group5_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
